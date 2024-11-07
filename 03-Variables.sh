66f7ac24d5864229677d85d191edd944f8c62da3
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-08 03:32:56 +0530

    Merge remote-tracking branch 'origin/main'

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -3,7 +3,19 @@
 
 # To access variable we use $variable or ${variable}
 
-trainer=john
+trainer=Steve
 
 echo DevOps Trainer - $trainer
 echo AWS Trainer - $trainer
+
+# Variable name can comprise of - a-z, A-Z, 0-9,
+# OS Background people use Capital Letter for Variable name
+# Java Background people use Camel case for Variable name
+# Python Background people use snake case for Variable nam
+
+# Data - Free to give anything, No data type, Everything is string.
+# In case if we have special characters in data, then do use double quotes.
+# example - trainer - "John Jacob"
+
+
+
