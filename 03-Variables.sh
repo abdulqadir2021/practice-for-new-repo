091da186d6892636720b3f29d6206bf52012915b
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-08 03:31:44 +0530

    Merge remote-tracking branch 'origin/main'

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -1,9 +1,9 @@
 # If we assigned a name to a set of data is called as variable
-# variable-data
+# variable=data
 
 # To access variable we use $variable or ${variable}
 
-trainer-john
+trainer=john
 
 echo DevOps Trainer - $trainer
 echo AWS Trainer - $trainer
