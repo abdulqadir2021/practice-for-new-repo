42066dc30b220073c29601a8553f56697f6be3c9
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-13 03:11:03 +0530

    Merge remote-tracking branch 'origin/main'

--- 06-if-condtion.sh
+++ 06-if-condtion.sh
@@ -34,3 +34,24 @@
 
 # Files
 # Operators : https://www.man7.org/linux/man-pages//man1/bash.1.html
+
+# We have one more condition - case
+# case $i in
+#abc)
+#  commands
+#  ;;
+#xyz)
+#  commands
+#  ;;
+#esac
+
+# However we don't prefer to do much with case command, because it can do only string comparison like two strings are equal or not equal
+fruit=orange
+case $fruit in
+ orange)
+   echo Available Quantity - 100
+   ;;
+ banana)
+   echo Available Quantity - 200
+   ;;
+ esac
