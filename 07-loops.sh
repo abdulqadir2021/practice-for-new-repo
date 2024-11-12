a679e8923c9cd64e31c2636c8dbdfc9f53ab1243
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-13 04:12:19 +0530

    Merge remote-tracking branch 'origin/main'

--- 07-loops.sh
+++ 07-loops.sh
@@ -13,4 +13,9 @@ x=10
 while [ $x -gt 0 ]; do
   echo Value x - $x
   x=$(($x-1))
+  if [$x -eq 5 ]; then
+    break
+  fi
 done
+
+# break command is to break and come out of loop. If we use exit it will exit the whole script, but break will just break the loop and next commands after the loop will be executed.
